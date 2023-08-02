d389cd0b780e323aa7efa145a8f97d01f074cd5c
CESAR NUNES DE OLIVEIRA <ra22139193-5@aluno.unicesumar.edu.br>
2023-08-02 13:21:29 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cor: {
+          primaria: "#ff6b6b" /* Vermelho */,
+          secundaria: "#0a1831" /* Amarelo */,
+          fundo: "#f3f4ed" /* Bege */,
+          texto: "#333333" /* Preto */,
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
