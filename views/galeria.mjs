06d4304f55ea124a7fb0ebd30e67537d4ce53e3d
CESAR NUNES DE OLIVEIRA <ra22139193-5@aluno.unicesumar.edu.br>
2023-08-21 11:48:51 -0300

    Some mt for better layout

--- views/galeria.mjs
+++ views/galeria.mjs
@@ -1,5 +1,5 @@
 const renderGaleria = () => {
-  return `<h1 class="text-3xl font-bold text-center text-red-500">Em construção</h1>`;
+  return `<h1 class="my-20 text-3xl font-bold text-center text-red-500">Em construção</h1>`;
 };
 
 export default renderGaleria;
